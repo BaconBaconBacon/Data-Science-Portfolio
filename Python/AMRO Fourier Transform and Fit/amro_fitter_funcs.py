96a35f53db9159a7038462f35e8e1b71db53325c
james <jdcf50@gmail.com>
2025-12-12 19:21:13 -0800

    Trying to improve the fitter's functioning. The covariance matrix is too often singular, so I implemented code to scale the data and mean parameter by the maximum value in the oscillation. It will also attempt a re-fit by removing the bounds of the lmfit Parameter objects. Needs more TLC.

--- Python/AMRO Fourier Transform and Fit/amro_fitter_funcs.py
+++ Python/AMRO Fourier Transform and Fit/amro_fitter_funcs.py
@@ -41,32 +41,20 @@ class AMROFitter:
         self.act_choices = self._get_H_T_values()
 
         # Save name info
-        self.save_name = save_name
-        self.fit_params_fp = os.path.join("Data", save_name + "_fit_params.csv")
-        self.results_fp = os.path.join("Data", save_name + "_results.pkl")
-        # may be redundant, could probably get it from the fit_params_df
-        self.fit_amps_fp = os.path.join("Data", save_name + "_fit_amps.csv")
+        s = "_ratio_{}_maxf_{}_".format(min_amp_ratio, max_freq)
+        self.save_name = save_name + s.replace(".", "p")
+        self._create_save_paths()
 
-        load_conds = (
-            (os.path.exists(self.fit_params_fp))
-            & os.path.exists(self.results_fp)
-            & os.path.exists(self.fit_amps_fp)
-        )
-        if load_conds:
-            print("Loading previous fit results.")
-            with open(self.results_fp, "rb") as f:
-                self.lmfit_results_objs = pickle.load(f)
-
-            self.fit_params_df = pd.read_csv(self.fit_params_fp)
-            self.fit_amps_df = pd.read_csv(self.fit_amps_fp)
-        else:
-            print("Initializing.")
-            self.lmfit_results_objs = {}
-            self.fit_params_df = pd.DataFrame()
-            self.fit_amps_df = pd.DataFrame()
-
+        self._read_or_initialize()
         self.verbose = verbose
 
+        return
+
+    def _create_save_paths(self):
+        self.fit_params_fp = os.path.join("Data", self.save_name + "_fit_params.csv")
+        self.results_fp = os.path.join("Data", self.save_name + "_results.pkl")
+        # may be redundant, could probably get it from the fit_params_df
+        self.fit_amps_fp = os.path.join("Data", self.save_name + "_fit_amps.csv")
         return
 
     def SineBuilder(
@@ -78,21 +66,6 @@ class AMROFitter:
         )
 
         return mean * (summation + 1)
-
-    def _filter_guess_params(self, guess_params: pd.DataFrame) -> pd.DataFrame:
-        """
-        Filter the fit parameter initial guesses from the Fourier transform
-        using the given parameters.
-
-        The amp_ratio is the ratio of the amplitude of a given frequency divided
-        by that of the strongest frequency's amplitude.
-
-        The freq is the number of oscillations per rotation of the sample.
-        """
-        q = "amp_ratio > {} & `freqs (cycles/rot)`<{}".format(
-            self.min_amp_ratio, self.max_freq
-        )
-        return guess_params.query(q)
 
     def ObjFcn(self, params, angle, res_data):
         """
@@ -106,105 +79,8 @@ class AMROFitter:
         res_model = self.SineBuilder(angle, amps_list, freqs_list, phase_list, offset)
 
         # Want to minimize least squares
-        return (res_model - res_data) ** 2
-
-    def FitAMROData(
-        self, ACT: str, H: int | float, T: int | float  # f_list: list,
-    ) -> lm.minimizer.MinimizerResult:
-        """ """
-
-        # Select the experimental data to be fitted
-
-        fit_df = self.GetAMROData(ACT, H, T)
-        guess_df = self._get_freqs_guesses(ACT, H, T)
-
-        # Extract data we are going to fit
-        x = fit_df["Sample Position (rads)"].values
-        y = fit_df["Res. (ohm-cm)"].values
-        y_mean = y.mean()
-
-        initial_params = self._initialize_parameters(guess_df, y_mean)
-
-        # Perform the minimization
-        minner = lm.Minimizer(self.ObjFcn, initial_params, fcn_args=(x, y))
-        results = minner.minimize()
-
-        if self.verbose:
-            print(lm.fit_report(results, show_correl=False))
-
-        del self.current_f_list
-        del self.current_f_list_str
-
-        return results
-
-    def _get_freqs_guesses(self, act, h, t):
-        q = 'ACT_str == "{}" & H == {} & T== {}'.format(  #' & `freqs (cycles/rot)` in @f_list'.format(
-            act, h, t
-        )
-        guess_df = self.ft_results_df.query(q)
-        self.current_f_list = guess_df["freqs (cycles/rot)"].unique()
-        self.current_f_list_str = [str(f) for f in self.current_f_list]
-
-        return guess_df
-
-    def _initialize_parameters(
-        self, guesses: pd.DataFrame, mean: float
-    ) -> lm.Parameters:
-        """"""
-
-        freqs_list = self.current_f_list  # guesses["freqs (cycles/rot)"].unique()
-
-        # Generate a Parameters ordered dictionary, to which we add Parameter objects
-        initial_p_guesses = lm.Parameters()
-        initial_p_guesses.add("mean", value=mean)
-
-        # Append all Parameter objects, except for the last one (must deal with appended 2)
-        i = 0
-        while i < (len(freqs_list) - 1):  # Extra 2 will always be at the end of f_list
-            freq = int(freqs_list[i])
-            self._add_freq_parameter(freq, initial_p_guesses, mean, guesses)
-            i += 1
-
-        # Deal with the final element
-        # TODO: Surely this is unnecessary?
-        if len(freqs_list) == len(guesses):
-            # If nothing has been appended, just add the information for the final FT guess
-            freq = int(freqs_list[i])
-            self._add_freq_parameter(freq, initial_p_guesses, mean, guesses)
-
-        return initial_p_guesses
-
-    def _add_freq_parameter(
-        self,
-        frequency: int,
-        params: lm.Parameters,
-        mean_val: float,
-        guesses_df: pd.DataFrame,
-    ) -> None:
-        """
-        Forcing all amplitudes to be positive, negative values show up as
-        a pi-sized phase offset.
-        """
-        temp_df = guesses_df.query("`freqs (cycles/rot)` == {}".format(frequency))
-        params.add(
-            "amp" + str(frequency),
-            value=temp_df["mag (ohm-cm)"].values[0] / mean_val,
-            min=0,
-        )
-
-        params.add(
-            "freq" + str(frequency),
-            value=temp_df["freqs (cycles/rot)"].values[0],
-            vary=False,
-        )
-
-        params.add(
-            "phase" + str(frequency),
-            value=temp_df["phase"].values[0],
-            min=-2 * np.pi,
-            max=2 * np.pi,
-        )
-        return
+        # return (res_model - res_data) ** 2
+        return res_model - res_data
 
     def FitACTExperiment(self, label):
         """"""
@@ -231,14 +107,163 @@ class AMROFitter:
 
         return
 
-    def _save_to_disk(self):
-        """ """
-        with open(self.results_fp, "wb") as f:
-            pickle.dump(self.lmfit_results_objs, f)
+    def FitAMROData(
+        self, ACT: str, H: int | float, T: int | float  # f_list: list,
+    ) -> lm.minimizer.MinimizerResult:
+        """
+        This function prepares the data of the AMR oscillation for fitting,
+         then fits it. To improve the fitting, the data and parameters are
+         scaled by the maximum resistivity of the AMRO oscillation. After
+         fitting, they are scaled back up. This is to avoid the minimzer
+         forgoing an error estimation for any given fit parameter.
+        """
 
-        self.fit_params_df.to_csv(self.fit_params_fp, sep=",")
-        self.fit_amps_df.to_csv(self.fit_amps_fp, sep=",")
+        # Select the experimental data to be fitted
+
+        fit_df = self.GetAMROData(ACT, H, T)
+        guess_df = self._get_freqs_guesses(ACT, H, T)
+
+        # Extract data we are going to fit
+        x = fit_df["Sample Position (rads)"].values
+        y = fit_df["Res. (ohm-cm)"].values
+
+        y_norm, y_scale = self._normalize_data(y)
+        y_mean = y_norm.mean()
+
+        initial_params = self._initialize_parameters(guess_df, y_mean)
+
+        # TODO: some parameters have No errors, causing NaNs later on.
+        # Perform the minimization
+        minner = lm.Minimizer(self.ObjFcn, initial_params, fcn_args=(x, y_norm))
+        results = minner.minimize()
+
+        # Check if the covariant matrix is singular
+        for param_name in results.params.keys():
+            if results.params[param_name].stderr is None:
+                print(results.params)
+                print(
+                    "Fit's covariance matrix is singular. Removing bounds and re-fitting."
+                )
+                for name in results.params:
+                    # if name.startswith("phase"):
+                    results.params[name].min = -np.inf
+                    results.params[name].max = np.inf
+                results = minner.minimize()
+
+        # Scale the fit parameters back up
+        if results.params["mean"].stderr is not None:
+            results.params["mean"].stderr *= y_scale
+        else:
+            raise ValueError("Mean parameter's error could not be calculated.")
+
+        for param_name in results.params.keys():
+            if param_name == "mean":
+                results.params[param_name].value *= y_scale
+                if results.params[param_name].stderr is not None:
+                    results.params[param_name].stderr *= y_scale
+                else:
+                    print(results.params)
+                    raise ValueError(
+                        f"{param_name} parameter's error could not be estimated."
+                    )
+            else:
+                if results.params[param_name].stderr is None:
+                    print(results.params)
+                    raise ValueError(
+                        f"{param_name} parameter's error could not be estimated."
+                    )
+
+        if self.verbose:
+            print(lm.fit_report(results, show_correl=False))
+            print()
+        del self.current_f_list
+        del self.current_f_list_str
+
+        return results
+
+    def _add_parameter(
+        self,
+        frequency: int,
+        params: lm.Parameters,
+        mean_val: float,
+        guesses_df: pd.DataFrame,
+        # y_scale: float,
+    ) -> None:
+        """
+        Forcing all amplitudes to be positive, negative values show up as
+        a pi-sized phase offset.
+
+        The y_scale scaling is to improve the error estimation of the fitter.
+
+        We divide the magnitude by the mean in order to align with the way
+        the SineBuilder function works.
+        """
+        temp_df = guesses_df.query("`freqs (cycles/rot)` == {}".format(frequency))
+        params.add(
+            "amp" + str(frequency),
+            value=temp_df["mag (ohm-cm)"].values[0] / mean_val,
+            min=0,
+        )
+
+        params.add(
+            "freq" + str(frequency),
+            value=temp_df["freqs (cycles/rot)"].values[0],
+            vary=False,
+        )
+
+        params.add(
+            "phase" + str(frequency),
+            value=temp_df["phase"].values[0]  # ,
+            # min=-2 * np.pi,
+            # max=2 * np.pi,
+        )
         return
+
+    def _normalize_data(self, y):
+        """Normalize y-values to O(1) for better numerical conditioning."""
+        y_scale = np.abs(y).max()
+        if y_scale < 1e-10:
+            y_scale = 1.0
+        return y / y_scale, y_scale
+
+    def _initialize_parameters(
+        self,
+        guesses: pd.DataFrame,
+        mean: float,
+    ) -> lm.Parameters:
+        """Note that the value of the 'mean' value should have been normalized"""
+
+        freqs_list = self.current_f_list  # guesses["freqs (cycles/rot)"].unique()
+
+        # Generate a Parameters ordered dictionary, to which we add Parameter objects
+        initial_p_guesses = lm.Parameters()
+        initial_p_guesses.add("mean", value=mean, min=0)
+
+        # Append all Parameter objects, except for the last one (must deal with appended 2)
+        i = 0
+        while i < (len(freqs_list) - 1):  # Extra 2 will always be at the end of f_list
+            freq = int(freqs_list[i])
+            self._add_parameter(freq, initial_p_guesses, mean, guesses)
+            i += 1
+
+        # Deal with the final element
+        # TODO: Surely this is unnecessary?
+        if len(freqs_list) == len(guesses):
+            # If nothing has been appended, just add the information for the final FT guess
+            freq = int(freqs_list[i])
+            self._add_parameter(freq, initial_p_guesses, mean, guesses)
+
+        return initial_p_guesses
+
+    def _get_freqs_guesses(self, act, h, t):
+        q = 'ACT_str == "{}" & H == {} & T== {}'.format(  #' & `freqs (cycles/rot)` in @f_list'.format(
+            act, h, t
+        )
+        guess_df = self.ft_results_df.query(q)
+        self.current_f_list = guess_df["freqs (cycles/rot)"].unique()
+        self.current_f_list_str = [str(f) for f in self.current_f_list]
+
+        return guess_df
 
     def GetAMROData(self, act=None, h=None, t=None):
         """
@@ -265,7 +290,7 @@ class AMROFitter:
         show_residuals=True,
         y_scale=1,
         y_label="Res. ch (ohm-cm)",
-        x_label="Angle (rads)",
+        x_label="Angle (deg)",
         sns_context="poster",
         delta=False,
         marker_size=60,
@@ -324,8 +349,10 @@ class AMROFitter:
                 fit_params = result.params  # .valuesdict()
                 plot_df = data_df.query("H=={} & T =={}".format(H, T))
 
-                # Note: Do not use x_label and y_label for this
+                # Note: Do not use vars x_label and y_label for this
                 x = plot_df["Sample Position (rads)"].values
+                x_plot = plot_df["Sample Position (deg)"].values
+
                 y = plot_df["Res. (ohm-cm)"].values
 
                 _ = self._get_freqs_guesses(act_choice, H, T)
@@ -364,22 +391,25 @@ class AMROFitter:
                     ax_resid = fig.add_subplot(gs[i * 2 + 1, j], sharex=ax_fit)
                     axes[i, j] = (ax_fit, ax_resid)
 
+                    ax_fit.set_xticks([0, 90, 180, 270, 360])
+                    ax_resid.set_xticks([0, 90, 180, 270, 360])
+
                     # Plot data and fit
                     sns.scatterplot(
-                        x=x,
+                        x=x_plot,
                         y=y,
                         color=H_PALETTE[H],
                         ax=ax_fit,
                         linewidth=0,
                         s=marker_size,
                     )
-                    sns.lineplot(x=x, y=y_fit, color="black", ax=ax_fit)
+                    sns.lineplot(x=x_plot, y=y_fit, color="black", ax=ax_fit)
                     ax_fit.set_xlabel("")
                     ax_fit.tick_params(labelbottom=False)
 
                     # Plot residuals
                     sns.scatterplot(
-                        x=x,
+                        x=x_plot,
                         y=residuals,
                         ax=ax_resid,
                         color="black",
@@ -387,22 +417,36 @@ class AMROFitter:
                         s=marker_size,
                     )
 
+                    # x labels
                     if i == (n_rows - 1):
                         ax_resid.set(xlabel=x_label)
-                    elif i == 0:
+                    else:
+                        ax_resid.set(xlabel="")
+                        ax_resid.tick_params(labelbottom=False)
+
+                    # titles
+                    if i == 0:
                         ax_fit.set_title(str(T).replace(".0", "") + "K")
 
+                    # y labels
                     if j == 0:
                         ax_fit.set(ylabel=y_label)
-                        # ax_resid.set(ylabel=r"$y-\hat{y}$ (%)")
-
+                    else:
+                        ax_fit.set(ylabel=None)
                 else:
                     ax = axes[i, j]
                     sns.scatterplot(
-                        x=x, y=y, color=H_PALETTE[H], ax=ax, linewidth=0, s=marker_size
+                        x=x_plot,
+                        y=y,
+                        color=H_PALETTE[H],
+                        ax=ax,
+                        linewidth=0,
+                        s=marker_size,
                     )
-                    sns.lineplot(x=x, y=y_fit, color=H_PALETTE[H], ax=ax)
+                    sns.lineplot(x=x_plot, y=y_fit, color=H_PALETTE[H], ax=ax)
                     ax.set_title(f"Position ({i}, {j})", fontsize=10)
+                    ax.set_xticks([0, 90, 180, 270, 360])
+
                     # ax.legend(fontsize=8)
                     if i == (n_rows - 1):
                         ax.set(xlabel=x_label)
@@ -423,6 +467,15 @@ class AMROFitter:
         # plt.tight_layout()
         # plt.tight_layout(rect=[0, 0, 0.95, 1])
         return fig, axes
+
+    def _save_to_disk(self):
+        """ """
+        with open(self.results_fp, "wb") as f:
+            pickle.dump(self.lmfit_results_objs, f)
+
+        self.fit_params_df.to_csv(self.fit_params_fp, sep=",")
+        self.fit_amps_df.to_csv(self.fit_amps_fp, sep=",")
+        return
 
     def _convert_params_to_lists(
         self, params_obj: lm.Parameters
@@ -451,6 +504,21 @@ class AMROFitter:
             params_dict["mean"],
         )
 
+    def _filter_guess_params(self, guess_params: pd.DataFrame) -> pd.DataFrame:
+        """
+        Filter the fit parameter initial guesses from the Fourier transform
+        using the given parameters.
+
+        The amp_ratio is the ratio of the amplitude of a given frequency divided
+        by that of the strongest frequency's amplitude.
+
+        The freq is the number of oscillations per rotation of the sample.
+        """
+        q = "amp_ratio > {} & `freqs (cycles/rot)`<{}".format(
+            self.min_amp_ratio, self.max_freq
+        )
+        return guess_params.query(q)
+
     def _get_init_params(self, act: str, T: str, H: str) -> pd.DataFrame:
         q = 'ACT_str == "{}" & H == {} & T == {}'.format(act, H, T)
 
@@ -475,21 +543,40 @@ class AMROFitter:
         except KeyError:
             return False
 
+    def _read_or_initialize(self):
+        load_conds = (
+            (os.path.exists(self.fit_params_fp))
+            & os.path.exists(self.results_fp)
+            & os.path.exists(self.fit_amps_fp)
+        )
+        if load_conds:
+            print("Loading previous fit results.")
+            with open(self.results_fp, "rb") as f:
+                self.lmfit_results_objs = pickle.load(f)
+
+            self.fit_params_df = pd.read_csv(self.fit_params_fp)
+            self.fit_amps_df = pd.read_csv(self.fit_amps_fp)
+        else:
+            print("Initializing.")
+            self.lmfit_results_objs = {}
+            self.fit_params_df = pd.DataFrame()
+            self.fit_amps_df = pd.DataFrame()
+
+        return
+
     def _pack_act_fit_results(
         self,
         lmfit_result,
-        # fits: pd.DataFrame,
-        # results: dict,
-        # fitted_amps: pd.DataFrame,
         act_label: str,
         t_label: str,
         h_label: str,
-        # f_list: list,
     ):
         """"""
+
         # Pack just amplitude parameters
         var_names = lmfit_result.var_names
         param_results = lmfit_result.params
+
         f_list = self._get_init_freqs(act_label, t_label, h_label)
 
         f_info = pd.DataFrame(
@@ -498,8 +585,7 @@ class AMROFitter:
                 "T (K)": t_label,
                 "H (T)": h_label,
                 "f_list": f_list,
-            }  # ,
-            # index=[0],
+            }
         )
         self.fit_amps_df = pd.concat([self.fit_amps_df, f_info])
 
@@ -517,10 +603,7 @@ class AMROFitter:
         params_df = pd.DataFrame(params_dict, index=[0])
         if params_df.isna().any().any():
             print(params_df)
-            raise
-            with pd.option_context("future.no_silent_downcasting", True):
-                params_df = params_df.fillna(0)
-            print("Filling NaN parameters.")
+            raise ValueError("NaN found in fitted parameters.")
 
         self.fit_params_df = pd.concat(
             [self.fit_params_df, params_df], ignore_index=True
