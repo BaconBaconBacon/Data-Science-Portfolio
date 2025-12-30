915b881d4d5b3305ce3451df1ede0c6d64b6496c
james <jdcf50@gmail.com>
2025-12-29 23:18:21 -0800

    Cleaned up the utils_fourier.py code by encapsulating some of the code blocks. Removed the unneccessary 'ACT' labels (leaving just the 'ACT_str' labels). Still would like to implement some code to compare the loaded fourier experiment labels against the amro_df labels (when the __init__ constructor loads an existing set of fourier transform results)

--- Python/AMRO Fourier Transform and Fit/plotting/plotting_misc.py
+++ Python/AMRO Fourier Transform and Fit/plotting/plotting_misc.py
@@ -1,3 +1,6 @@
 """
-Script for visualization functions.
+Script for visualization functions
+
+There will likely be code in the fitter function
+that can be used in the fourier and loader plotting scripts.
 """
